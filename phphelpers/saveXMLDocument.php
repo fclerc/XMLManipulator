821a05abe041baed43caf7f6c9882612dd7025d3
fclerc <fc@florianclerc.com>
2015-06-21 22:09:37 +0200

    Modifying the XML file saver

--- phphelpers/saveXMLDocument.php
+++ phphelpers/saveXMLDocument.php
@@ -8,17 +8,6 @@ if($_POST['file'] == $_POST['formerFile'] || !(file_exists($_POST['file']))){//n
     if($_POST['file'] != $_POST['formerFile'] && $_POST['formerFile']!=''){//if the user has renamed the file, delete the former File
         unlink($_POST['formerFile']);
     }
-    
-    //in case this is the file containing the resources, add the ids and re-generate the docs about resources
-    if($doc->getElementsByTagName('resourcesStructure')->length > 0){
-        //adding ids to new elements
-        include 'idAdderRunner.php';
-        $x->addIdsToXML($_POST['file'], 'R', '','continue', 'all');
-        
-        //updating data about the resources
-        include 'getDataFromResourceStructure.php';
-        $t->generateJSONFromDoc($_POST['file'], '../data/infos/resourcesData.json');
-    }
 }
 else{//the filename already exists, and it is not the currently edited file : abort
         header('HTTP/1.1 200');
@@ -26,6 +15,4 @@ else{//the filename already exists, and it is not the currently edited file : ab
         die(json_encode(array('message' => 'RENAMEERROR')));
 
 }
-    
-
 ?>
