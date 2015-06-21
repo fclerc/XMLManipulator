f4feef68c7de6a8e352595022df748d5d0e012c5
fclerc <fc@florianclerc.com>
2015-06-21 21:30:24 +0200

    Adding readme file with first contents

---
+++ README.md
@@ -0,0 +1,7 @@
+Use this code to edit XML files in your favorite Web Browser.
+
+More details in the next few days (by the end of June 2015)
+
+
+
+
--- fileValuesModification.php
+++ fileValuesModification.php
@@ -1,6 +1,3 @@
-<?php session_start(); 
-    require_once 'phphelpers/langFinder.php';
-?>
 <!DOCTYPE HTML>
 <!-- This file uses XMLManipulator in order to enable the user to change the values of the XML file he is using. Name and path of the file are sent by a POST form  -->
 <html>
@@ -8,30 +5,14 @@
 		<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
         <link href="css/bootstrap.css" type="text/css" rel="stylesheet"/>
         <link href="css/XMLManipulator.css" type="text/css" rel="stylesheet"/>
-        <link href="css/main.css" type="text/css" rel="stylesheet"/>
-        
     </head>
     
     <body>
-    <?php 
-    //I store these variables here to call them later...could be easier to use directly $_POST['path'],... in the rest of the script but idiot php tells me these indexes no longer exist in the array (well in fact sometimes there is no problem, but some other times, in EXACTLY the same conditions, it tells me they don't exist (even if it is possible to echo and display these array values in the html page, it doesn't let me use it in the other parts of the script)...
-    if(!isset($_POST['path'])){//values are in the session variable (sent by the fileHandler.php)
-        $path = $_SESSION['path'];
-        $file = $_SESSION['file'];
-        $scales = $_SESSION['scales'];
-        $section = $_SESSION['section'];
-    }
-    else{//files are in the post, directly sent by index.php
-        $path = $_POST['path'];
-        $file = $_POST['file'];
-        $scales = $_POST['scales'];
-        $section = $_POST['section'];
-    }
-    ?>
+
 		<div class="container">
-			<h1><span id="valuesPageTitle">valuesModification.h1</span><span id="sectionName"><?php echo $section; ?></span><small><span id="currentFile">valuesModification.currentFileIntro</span><span id="currentFileName"><?php echo $file; ?></span></small></h1>
-			<p id="generalInstructions">valuesModification.instructions</p>
-            <a href="index.php" id="mainLink">common.back</a>
+			<h1>XMLManipulator Demo Page</h1>
+			<p>This page is a demo of the XMLManipulator tool you can use to edit your XML files directly in your web browser. <a href = "https://github.com/fclerc/XMLManipulator">Code available on my Github</a></p>
+
 			<div id="XMLcontainer"></div>
         </div>
         
@@ -45,18 +26,7 @@
         <script type="text/javascript">
         $(function(){    
             
-            var translationFile = 'translation/'+<?php echo "'".$lang."'"; ?>+'.json';
-            $.ajax({//loading translation
-                type: "GET",
-                url: translationFile,
-                success: function(data){
-                    _.setTranslation(data);
-                    
-                    $('#valuesPageTitle, #currentFile, #generalInstructions, #mainLink, #sectionName').each(function(){
-                        $(this).text(_($(this).text()));
-                    });
-                    
-                    
+            var translationFile = 'translation/'+<?php echo "'".$lang."'"; ?>+'.
                     //getting the file and corresponding scales, and calling the function to display XML
                     var file = <?php echo "'".$path."/".$file."'"; ?>;
                     var scales = <?php if($scales!=''){echo file_get_contents($scales);}else{echo '""';} ?>;
